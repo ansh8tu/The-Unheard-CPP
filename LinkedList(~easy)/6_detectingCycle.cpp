b7f243d97ad7a7c93aacd8a1ed1952856c9e76a7
ansh8tu <anshu8tu@gmail.com>
2021-04-06 04:30:26 +0530

    Added the concept of rabbit and hare lol!

---
+++ LinkedList(~easy)/6_detectingCycle.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    public:
+        Node(int valueToStore){
+            data = valueToStore;
+            next = NULL;
+        }
+};
+
+bool detectCycle(Node* head){
+
+    //base case
+    if(head==NULL || head->next ==NULL){
+        return false;
+    }
+
+    Node* slowPtr = head;
+    Node* fastPtr = head;
+
+    //if you are writing fastPtr->next!=NULL && fastPtr!=NULL then dont't try optimising it to this
+    //else you'll get into troubles at LeetCode/CP
+
+    //you'll get this kinda error
+    //runtime error: member access within null pointer of type 'Node' (solution.cpp)
+    //SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior prog_joined.cpp:25:24
+    while(fastPtr->next && fastPtr){
+        fastPtr= fastPtr->next->next;
+        slowPtr = slowPtr->next;
+        
+        //cycle present if these two meet at any point
+        if(fastPtr == slowPtr){
+            return true;
+        }
+    }
+
+    return false;
+
+
+}
+
+void printList(Node* node){
+    while(node !=NULL){
+        cout<<node->data<<"->";
+        node = node->next;
+    }
+    cout<<"NULL";
+}
+
+int main(){
+    Node* head = NULL;
+    Node* firstNode = new Node(10);
+    Node* secondNode = new Node(20);
+    Node* thirdNode = new Node(30);
+    Node* fourthNode = new Node(40);
+    Node* fifthNode = new Node(50);
+
+    head = firstNode;
+    firstNode->next = secondNode;
+    secondNode->next = thirdNode;
+    thirdNode->next = fourthNode;
+    fourthNode->next = fifthNode;
+
+    //to make a cycle in order to check that our code is working properly or not
+    fifthNode->next = head;
+
+    cout<< detectCycle(head) << endl;
+    //returns 1 in this case as loop present
+}
