3919296130f5db74d58b05844e806c1ad3b96612
ansh8tu <anshu8tu@gmail.com>
2021-07-07 06:37:36 +0530

    Added improved code with more testCases from LeetCode!

--- theStack(~idkMan)/9_balancedParenthesis.cpp
+++ theStack(~idkMan)/9_balancedParenthesis.cpp
@@ -2,58 +2,66 @@
 #include<stack>
 using namespace std;
 
-bool balancedCheck(string ex){
-    bool res = true;
+bool isValid(string ex) {
     stack<char> stackzy;
 
     for(int i=0; i<ex.length(); i++){
 
         if(ex[i] == '(' || ex[i] == '{' || ex[i] == '['){
-            stackzy.push(ex[i]);
+                stackzy.push(ex[i]);
         }
-
-        else if(ex[i] == ')'){
-            if(stackzy.top() == ')'){
-                stackzy.pop();
-            }
             
-            else{
-                res = false;
-                break;
+        else{
+                
+            if(stackzy.empty()){
+                return false;
             }
-        }
-
-        else if(ex[i] == '}'){
-            if(stackzy.top() == '}'){
-                stackzy.pop();
-            }
+                
+            else if(ex[i] == ')'){
+                if(stackzy.top() == '('){
+                    stackzy.pop();
+                }
             
-            else{
-                res = false;
-                break;
+                else{
+                    return false;
+                }
             }
-        }
 
-        else if(ex[i] == ']'){
-            if(stackzy.top() == ']'){
-                stackzy.pop();
-            }
+            else if(ex[i] == '}'){
+                if(stackzy.top() == '{'){
+                    stackzy.pop();
+                }
             
-            else{
-                res = false;
-                break;
+                else{
+                    return false;
+                }
             }
-        }
+
+            else if(ex[i] == ']'){
+                if(stackzy.top() == '['){
+                    stackzy.pop();
+                }
+            
+                else{
+                    return false;
+                }
+            }
         
+            }
+
+           
+        }
+
+        if (stackzy.empty()){
+            return true;
+        }
+        return false;
+
     }
 
-    return res;
-
-}
-
 int main(){
-    string ex = "{[(])}";
-    bool res = balancedCheck(ex);
+    string ex = "({])";
+    bool res = isValid(ex);
 
     if(res){
         cout<<"Balanced Parenthesis!"<< endl;
