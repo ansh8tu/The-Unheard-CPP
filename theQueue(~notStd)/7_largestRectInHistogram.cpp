fee8586e3a6f73c2ddd0e0967d796beaa01da49f
ansh8tu <anshu8tu@gmail.com>
2021-07-13 15:46:30 +0530

    Leetcode84!

---
+++ theQueue(~notStd)/7_largestRectInHistogram.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+
+    stack<int> stackzy;
+    int i=0;
+    int s = heights.size();
+    int ans = 0;
+    heights.push_back(0);
+
+    while(i<s){
+
+        while(!stackzy.empty() && heights[stackzy.top()] > heights[i]){
+            int top = stackzy.top();
+            int h =  heights[stackzy.top()];
+            stackzy.pop();
+
+            if(stackzy.empty()){
+                ans = max(ans, h*i);
+            }
+
+            else{
+                int len = i - stackzy.top() -1;
+                ans = max(ans, h*len);
+            }
+        }
+
+        stackzy.push(i);
+        i++;    
+    }
+    return ans;
+
+}
+
+int main(){
+
+    vector<int> heights = {2,1,5,6,2,3};
+    int ans = largestRectangleArea(heights);
+    cout<<"The area of largest rectangle is: "<<ans<<endl;
+
+}
