25848a744326f2a22fcbf67144519b7b9d7d7230
ansh8tu <anshu8tu@gmail.com>
2021-09-30 19:41:56 +0530

    This one's for CP!

---
+++ theBoilerPlate(~idkWhy)/header.h
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> si;
+typedef unordered_set<int> usi;
+typedef map<int,int> mi;
+typedef unordered_map<int,int> umi;
+typedef stack<int> st;
+typedef queue<int> que;
+typedef priority_queue<int> pq;
+
+
+#define LOOP(i, a, b) for(int i = a; i<b; i++) 
+#define ITLOOP(it, a, b) for(auto it=a; it!=b; it++)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
